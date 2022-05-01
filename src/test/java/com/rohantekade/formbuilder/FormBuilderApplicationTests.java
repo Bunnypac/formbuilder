3614b59e42a9a3df7464490137df464f78bc7067
rohan <Rohantekade64@gmail.com>
2022-05-01 21:47:47 +0530

    Initial commit

---
+++ src/test/java/com/rohantekade/formbuilder/FormBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohantekade.formbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
